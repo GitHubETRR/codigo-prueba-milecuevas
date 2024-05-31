675d9bc12a99be8635c655c05154a4a8c53d2727
milecuevas <mcuevas@etrr.edu.ar>
2024-05-31 15:34:08 -0300

    commit 1

---
+++ ACT32.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int num;
+    printf("ingrese un numero\n");
+    scanf("%d",&num);
+    num = num%2;
+    if(num==0){
+        printf("su numero ingresado es par");
+    }else{
+        printf("su numero ingresado es impar");
+    }
+
+return 0;
+}
