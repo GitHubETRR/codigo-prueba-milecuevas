0e7a402f253a86a67bd0b1c80f80d0fd0f5f576b
milecuevas <mcuevas@etrr.edu.ar>
2024-06-14 10:23:21 -0300

    commit 1

--- ACT35.c
+++ ACT35.c
@@ -6,14 +6,14 @@ int main (void)
     printf("ingrese un numero de 4 digitos\n");
     scanf("%d",&num);
     int num1 = num/1000;
-    int num2 = num1/10;
+    int num2 = (num/100)%10;
     int num3 = (num%100)/10;
     int num4 = (num%100)%10;  
-    if(num1==num4 && num3==num2);{
+    if((num1==num4) && (num3==num2)){
         printf("su numero ingresado es capicua\n");
     }else{
-        printf("su numero ingresado no es capicua\n")
+        printf("su numero ingresado no es capicua\n");
     }
-        
 
+    return 0;  
 }
