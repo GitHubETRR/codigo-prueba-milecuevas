b4f930443551f37b1d80a09a9b3edae80881663b
mile <mcuevas@etrr.edu.ar>
2025-05-15 12:48:36 -0300

    Update ACTF1.c
    .

--- SEXTO/ACTF1.c
+++ SEXTO/ACTF1.c
@@ -9,28 +9,29 @@ typedef struct{
     int numAuto;
     char escuderia[LIMITE];
     int puntCampPilotos;
-    int puntCampConstr;//revisar
+    //int puntCampConstr;
     struct pilotos *next;
 }pilotos_t;
 
 typedef enum{
-    salir,
-    agregarPilotos,//0
-    mostrarPilotos,//1
-    buscarPilotos,//2
-    modificarPilotos,//3
-    eliminarPilotos,//4
+    salir,//0
+    agregarPilotos,//1
+    mostrarPilotos,//2
+    buscarPilotos,//3
+    modificarPilotos,//4
+    eliminarPilotos,//5
 }opciones_t;
 
 int inicio();
 pilotos_t* agregarPiloto(pilotos_t *lista);
-void mostrarPilotos(pilotos_t *lista);
+void mostrarPiloto(pilotos_t *lista);
+void liberar (pilotos_t *lista);
+
 
 int main()
 {
     int opción; 
     pilotos_t *lista = NULL;
-
     do
     {
         opción=inicio();
@@ -40,21 +41,31 @@ int main()
                 lista = agregarPiloto(lista);
                 break;
             case mostrarPilotos:
-                mostrarPilotos(lista);
+                mostrarPiloto(lista);
+                break;
+            case buscarPilotos:
+                //funcion;
+                break;
+            case modificarPilotos:
+                //funcion;
+                break;
+            case eliminarPilotos:
+                //funcion;
                 break;
         }
     }while (opción!=salir);
+    liberar(lista);
     return 0;
 }
- 
+
     int inicio()
     {
     int opcion;
     printf("Bienvenido! Este programa es una base de datos de los pilotos de F1\n");
     printf("Ingrese:\n");
     printf("(1):Para agregar un piloto \n");
-    printf("(2):Mostrar un piloto\n");
-    printf("(3):Buscar un piloto\n");//REVISAR
+    printf("(2):Mostrar pilotos\n");
+    printf("(3):Buscar un piloto\n");
     printf("(4):Modificar los puntos de los campeonatos\n");
     printf("(5):Eliminar un piloto\n");
     printf("(0):Salir\n");
@@ -62,7 +73,8 @@ int main()
     return opcion;
     }
 
-pilotos_t * agregarPiloto(pilotos_t * lista){
+pilotos_t * agregarPiloto(pilotos_t * lista)
+    {
     pilotos_t *piloto_ptr = (pilotos_t *)malloc(sizeof(pilotos_t));
     if (piloto_ptr == NULL) {
         printf("No hay más memoria\n");
@@ -72,27 +84,40 @@ pilotos_t * agregarPiloto(pilotos_t * lista){
         scanf("%s",piloto_ptr->nombre);
         printf("ingrese el apellido del piloto:\n");
         scanf("%s",piloto_ptr->apellido);
-        printf("ingrese el númeor del auto del piloto:\n");
+        printf("ingrese el número del auto del piloto:\n");
         scanf("%d",&piloto_ptr->numAuto);
         printf("ingrese la escudería del piloto:\n");
         scanf("%s",piloto_ptr->escuderia);
         printf("ingrese los puntos en el campeonato de pilotos:\n");
         scanf("%d",&piloto_ptr->puntCampPilotos);
-        printf("ingrese los puntos en el campeonato de constructores:\n");//REVISAR
+        //printf("ingrese los puntos en el campeonato de constructores:\n");
         printf("Piloto agregado correctamente!\n\n");
         piloto_ptr->next = lista;
         return piloto_ptr;
+        }
     }
     
-void mostrarPilotos(pilotos_t *lista){
-        while (lista != NULL){
+void mostrarPiloto(pilotos_t *lista)
+    {
+        while (lista != NULL)
+        {
         printf("Nombre: %s\n", lista->nombre);
         printf("Apellido: %s\n",lista->apellido);
         printf("Número de auto: %d\n", lista->numAuto);
         printf("Escudería: %s\n", lista->escuderia);
         printf("Puntos campeonato de pilotos: %d\n", lista->puntCampPilotos);
-        printf("Puntos campeonato de constructores: %d\n", lista->puntCampConstr);//REVISAR
+        //printf("Puntos campeonato de constructores: %d\n\n", lista->puntCampConstr);
         lista = lista->next;;
         }
     }
-}
+
+void liberar (pilotos_t *lista)
+    {
+        pilotos_t *aux;
+        while(lista!=NULL)
+        {
+            aux=lista;
+            lista=lista->next;
+            free(aux);
+        }
+    }
